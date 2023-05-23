0bbed5b3389b66650423e3608d89c4663e3e4b17
Xernaxe <93713092+Xernaxe@users.noreply.github.com>
2023-05-23 18:50:27 +0200

    hero component width fix

--- themes/kickstartTheme/components/hero.php
+++ themes/kickstartTheme/components/hero.php
@@ -2,7 +2,7 @@
   <div class="heroWrapper">
 
     <div class="heroOverlay"></div>
-    <img src="<?php echo get_theme_file_uri('/images/heroimg.png')?>" alt="Hero Img">
+    <img class="heroIMG" src="<?php echo get_theme_file_uri('/images/heroimg.png')?>" alt="Hero Img">
   
     <h1 class="heroH">Welcome & Enjoy</h1>
 
